218f2687d52255703d1fa37f7444a5b7f990ad2d
Jonathan Antonio Felix da Silva <j.jonathanlima@hotmail.com>
2020-09-11 12:19:16 -0300

    Update navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,9 +32,9 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
+                    <li class="nav-item">
+                        <a class="nav-link" href=" {{ route('novels.index') }} ">Novels</a>
+                    </li>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
